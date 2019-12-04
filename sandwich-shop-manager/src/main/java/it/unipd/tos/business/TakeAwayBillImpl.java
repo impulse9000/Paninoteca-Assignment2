3354cae6a83cfb1d8bdb92e040d15f5d070e090d
Andrea Polo <andreapolo98@gmail.com>
2019-12-04 16:18:14 +0100

    aggiunto sconto 10% su totale(fritti+panini)>50 e relativi test, close #3

--- sandwich-shop-manager/src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ sandwich-shop-manager/src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -23,29 +23,59 @@ public class TakeAwayBillImpl implements TakeAwayBill {
 		while (items.hasNext()) {
 			finalPrice += items.next().getPrice();
 		}
-		return finalPrice-get50Discount(itemsOrdered);
+		return finalPrice-totalDiscounts(itemsOrdered, finalPrice);
 		
 	}
 	
 	// 50% di sconto sul panino meno caro se acquistati più di 5, ritorna il risparmio in euro oppure 0 se non applicabile
 	public double get50Discount(List<MenuItem> itemsOrdered) {
-		int count = 0;
-		double minPrice = 100000;
-		ListIterator<MenuItem> items = itemsOrdered.listIterator();
-		while(items.hasNext()) {
-			MenuItem menuItem = items.next();
-			if (menuItem.getItemType() == itemType.PANINO) {
-				count++;
-				if (minPrice > menuItem.getPrice()) {
-					minPrice = menuItem.getPrice();
+		if (itemsOrdered.size()>5) {
+			int count = 0;
+			double minPrice = 100000;
+			ListIterator<MenuItem> items = itemsOrdered.listIterator();
+			while(items.hasNext()) {
+				MenuItem menuItem = items.next();
+				if (menuItem.getItemType() == itemType.PANINO) {
+					count++;
+					if (minPrice > menuItem.getPrice()) {
+						minPrice = menuItem.getPrice();
+					}
 				}
 			}
-			
+			if(count>5)
+				return 0.50*minPrice;
 		}
-		if (count>5) 
-			return 0.50*minPrice;
-		else
-			return 0;
+		return 0;
+	}
+	
+	// 10% di sconto se il totale dell'ordine supera i 50 euro (contando solo panini e fritti)
+	// ritorna  risparmio in euro oppure 0 se non applicabile
+	
+	public double get10Discount(List<MenuItem> itemsOrdered, double totalOrderPrice) {
+		if (totalOrderPrice>50) {
+			double totalFilteredPrice = 0;
+			List<MenuItem> filteredList = new ArrayList<MenuItem>();
+			ListIterator<MenuItem> items = itemsOrdered.listIterator();
+			while(items.hasNext()) {
+				MenuItem menuItem = items.next();
+				if (menuItem.getItemType() == itemType.PANINO || menuItem.getItemType() == itemType.FRITTO) {
+					filteredList.add(menuItem);
+					totalFilteredPrice+=menuItem.getPrice();
+				}
+			}
+			if (totalFilteredPrice>50)
+				return totalOrderPrice*0.10;
+		}
+		return 0;
+	}
+	
+	
+	public double totalDiscounts(List<MenuItem> itemsOrdered, double totalOrderPrice) {
+		double totDisc = 0;
+		totDisc+=get50Discount(itemsOrdered);
+		totDisc+=get10Discount(itemsOrdered, totalOrderPrice);
+		return totDisc;
+		
 	}
 	
 
--- sandwich-shop-manager/src/test/java/it/unipd/tos/business/TestTakeAwayBillImpl.java
+++ sandwich-shop-manager/src/test/java/it/unipd/tos/business/TestTakeAwayBillImpl.java
@@ -17,14 +17,16 @@ import it.unipd.tos.model.MenuItem.itemType;
 
 public class TestTakeAwayBillImpl {
 	
-	MenuItem m1,m2,m3,m4,m5,m6,m7,m8,m9,m10;
-	List<MenuItem> lMista, lPanini;
+	MenuItem m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11;
+	List<MenuItem> lMista, lPanini, lPaniniFritti;
 	TakeAwayBillImpl bill; 
 	
 	@Before
 	public void before() {
-		lMista = new ArrayList<MenuItem>(); //lista con AL PIU' 5 panini
-		lPanini = new ArrayList<MenuItem>(); //lista con ALMENO 5 panini
+		lMista = new ArrayList<MenuItem>(); //lista con AL PIU' 5 panini, totale(misto)>50 euro, totale(fritti+panini)<=50 euro
+		lPanini = new ArrayList<MenuItem>(); //lista con ALMENO 5 panini, totale(fritti+panini)<=50 euro
+		lPaniniFritti = new ArrayList<MenuItem>(); // lista con AL PIU' 5 panini, totale(fritti+panini)>50 euro
+		
 		bill = new TakeAwayBillImpl();
 		m1 = new MenuItem(itemType.BEVANDA, "coca-cola", 2.0);
 		m2 = new MenuItem(itemType.FRITTO, "arancini", 4.5);
@@ -34,28 +36,65 @@ public class TestTakeAwayBillImpl {
 		m6 = new MenuItem(itemType.PANINO, "vegano", 4.5);
 		m7 = new MenuItem(itemType.PANINO, "4 formaggi", 3.5);
 		m8 = new MenuItem(itemType.PANINO, "salsiccia", 3.5);
-		m9 = new MenuItem(itemType.FRITTO, "crocchette patate", 2.5);
+		m9 = new MenuItem(itemType.FRITTO, "crocchette patate", 3.5);
 		m10 = new MenuItem(itemType.BEVANDA, "fanta", 2.0);
-		Collections.addAll(lMista, m1,m2,m3);
+		m11 = new MenuItem(itemType.PANINO, "il costoso", 45.0);
+		Collections.addAll(lMista, m1,m2,m11);
 		Collections.addAll(lPanini, m3,m4,m5,m6,m7,m8);
+		Collections.addAll(lPaniniFritti, m1,m2,m3,m9,m10,m11);
 	}
 	
 	@Test
 	public void testOrderPrice() throws TakeAwayBillException {
-		double expectedPriceMista = m1.getPrice() + m2.getPrice() + m3.getPrice();
-		double expectedPricePanini = m3.getPrice() + m4.getPrice() + m5.getPrice()/2 + m6.getPrice() + m7.getPrice() + m8.getPrice();
-		double sumPriceMista = bill.getOrderPrice(lMista);
-		double sumPricePanini = bill.getOrderPrice(lPanini);
-		assertEquals(expectedPriceMista,sumPriceMista, 0.00);
-		assertEquals(expectedPricePanini,sumPricePanini, 0.00);
+		double expectedPriceMista = 51.5; //totale con nessuno sconto dato che panini<=5 e totale(fritti+panini)<=50
+		double expectedPricePanini = 23.5-3.5*0.50; //totale con sconto 50% sul panino meno caro dato che panini>5
+		double expectedPricePaniniFritti = 61.5-61.5*0.10; //totale con sconto 10% sul totale(fritto+panino)>50
+		double actualPriceMista = bill.getOrderPrice(lMista);
+		double actualPricePanini = bill.getOrderPrice(lPanini);
+		double actualPricePaniniFritti = bill.getOrderPrice(lPaniniFritti);
+		assertEquals(expectedPriceMista,actualPriceMista, 0.00);
+		assertEquals(expectedPricePanini,actualPricePanini, 0.00);
+		assertEquals(expectedPricePaniniFritti, actualPricePaniniFritti, 0.00);
 	}
 	
 	@Test
 	public void testGet50Discount() {
-		double expectedDiscountMista = bill.get50Discount(lMista);
-		double expectedDiscountPanini = bill.get50Discount(lPanini);
-		assertEquals(0.00, expectedDiscountMista, 0.00);
-		assertEquals(3.50*0.50, expectedDiscountPanini, 0.00);
+		double expectedDiscountMista = 0.00; //nessuno sconto dato che panini<=5 
+		double expectedDiscountPanini = 3.5*0.50; //sconto 50% sul panino meno caro dato che panini>5
+		double expectedDiscountPaniniFritti = 0.00; //nessuno sconto dato che panini<=5 
+		double actualDiscountMista = bill.get50Discount(lMista);
+		double actualDiscountPanini = bill.get50Discount(lPanini);
+		double actualDiscountPaniniFritti = bill.get50Discount(lPaniniFritti);
+		assertEquals(expectedDiscountMista, actualDiscountMista, 0.00);
+		assertEquals(expectedDiscountPanini, actualDiscountPanini, 0.00);
+		assertEquals(expectedDiscountPaniniFritti, actualDiscountPaniniFritti, 0.00); 
+		
+	}
+	
+	@Test
+	public void testGet10Discount(){
+		double expectedDiscountMista = 0.00; //nessuno sconto dato che totale(fritti+panini)<=50
+		double expectedDiscountPanini = 0.00; ////nessuno sconto dato che totale(fritti+panini)<=50
+		double expectedDiscountPaniniFritti = 61.5*0.10; //sconto 10% sul totale dato che totale(fritti+panini)>50
+		double actualDiscountMista = bill.get10Discount(lMista, 56);
+		double actualDiscountPanini = bill.get10Discount(lPanini,23.5);
+		double actualDiscountPaniniFritti = bill.get10Discount(lPaniniFritti,61.50);
+		assertEquals(expectedDiscountMista, actualDiscountMista, 0.00);
+		assertEquals(expectedDiscountPanini, actualDiscountPanini, 0.00);
+		assertEquals(expectedDiscountPaniniFritti, actualDiscountPaniniFritti, 0.00);
+	}
+	
+	@Test
+	public void testTotalDiscounts() {
+		double expectedDiscountMista = 0.00; //nessuno sconto dato che panini<=5 e totale(fritti+panini)<=50
+		double expectedDiscountPanini = 3.50*0.50; //sconto 50% sul panino meno caro dato che panini>5
+		double expectedDiscountPaniniFritti = 61.5*0.10; //sconto 10% sul totale dato che totale(fritti+panini)>50
+		double actualDiscountMista = bill.totalDiscounts(lMista, 56);
+		double actualDiscountPanini = bill.totalDiscounts(lPanini, 23.5);
+		double actualDiscountPaniniFritti = bill.totalDiscounts(lPaniniFritti,61.50);
+		assertEquals(expectedDiscountMista, actualDiscountMista, 0.00);
+		assertEquals(expectedDiscountPanini, actualDiscountPanini, 0.00);
+		assertEquals(expectedDiscountPaniniFritti, actualDiscountPaniniFritti, 0.00);
 		
 	}
 	
